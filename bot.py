8205aa1c3dc4af2b3989809807424f617c387fff
NoobLose <pnobelik@mail.ru>
2020-07-11 22:03:04 +0300

    add timings, pua

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@ from os import environ
 from random import choice
 from time import sleep
 
-from utils.utils import declination_count, get_random_file
+from utils.utils import declination_count, get_random_file, text_has_emoji, date_representation, date_to_tuple
 
 import telebot
+import datetime
 
 # with open("token.txt", "r") as tk:
 #     TOKEN = tk.readline()
@@ -50,8 +51,18 @@ def add_new_player(user, chat):
         return False
     chats.insert_one(
         {"personal_id": user.id, "chat_id": chat.id, "score": 0, "sticker_reading_mode": False, "piu_count": 0,
-         "pua_count": 0, "wink_count": 0})
+         "pua_count": 0, "wink_count": 0, "emoji": None})
     return True
+
+
+def username_with_emoji(user, chat):
+    chats = dbase["chats"]
+
+    current_user = chats.find_one({"personal_id": user.id, "chat_id": chat.id})
+
+    if current_user["emoji"] is None:
+        return user.username
+    return "{} {}".format(user.username, current_user["emoji"])
 
 
 bot = telebot.TeleBot(TOKEN)
@@ -59,6 +70,7 @@ bot = telebot.TeleBot(TOKEN)
 cluster = MongoClient(MONGODB_TOKEN)
 dbase = cluster[MONGO_DB]
 standard_answers = dbase["standardAnswers"]
+timings = dbase["timings"]
 
 
 @bot.message_handler(commands=["register"])
@@ -72,7 +84,7 @@ def echo_register(message):
         result = "already_registered"
 
     sending_message, parse_mode = get_standard_answer(result)
-    bot.send_message(chat.id, sending_message.format(sender.username), parse_mode=parse_mode)
+    bot.send_message(chat.id, sending_message.format(username_with_emoji(sender, chat)), parse_mode=parse_mode)
 
 
 @bot.message_handler(commands=["pidor"])
@@ -82,32 +94,148 @@ def echo_pidor(message):
     participants = [bot.get_chat_member(chat.id, participant["personal_id"]) for participant in
                     chats.find({"chat_id": chat.id})]
 
+    if timings.find_one({"chat_id": chat.id, "game": "pidor"}) is None:
+        timings.insert_one(
+            {"game": "pidor", "chat_id": chat["chat_id"], "last_run": None, "delta_hours": 24, "delta_minutes": 0,
+             "pidor_of_the_day": None, "label_hours": ["час", "часа", "часов"],
+             "label_minutes": ["минута", "минуты", "минут"], "label_seconds": ["секунда", "секунды", "секунд"]})
+
     if len(participants) <= 0:
         sending_message, parse_mode = get_standard_answer("not_enough_players")
         bot.send_message(chat.id, sending_message, parse_mode=parse_mode)
+        return
+
+    timing = timings.find_one({"chat_id": chat.id, "game": "pidor"})
+    current_date = datetime.datetime.now()
+
+    if timing["last_run"] is None or timing["pidor_of_the_day"] is None:
+        timings.update_one({"chat_id": chat.id, "game": "pidor"}, {"$set": {"last_run": date_to_tuple(current_date)}})
     else:
-        pidor_messages = dbase["pidorMessages"]
+        last_run = datetime.datetime(*timing["last_run"])
+        delta = datetime.timedelta(hours=timing["delta_hours"], minutes=timing["delta_minutes"])
 
-        pidor_message = get_random_file(pidor_messages, {})
+        if last_run + delta > current_date:
+            sending_message, parse_mode = get_standard_answer("still_early_pidor")
+            bot.send_message(chat.id, sending_message.format(
+                username_with_emoji(bot.get_chat_member(chat.id, timing["pidor_of_the_day"]).user, chat),
+                date_representation(last_run + delta - current_date, timing)), parse_mode=parse_mode)
+            return
+        else:
+            timings.update_one({"chat_id": chat.id, "game": "pidor"},
+                               {"$set": {"last_run": date_to_tuple(last_run + delta)}})
 
-        for text in pidor_message["intro_messages"]:
-            if text == "send_intro_sticker":
-                bot.send_sticker(chat.id, get_random_intro_sticker()["file_id"])
-            else:
-                bot.send_message(chat.id, text, parse_mode=pidor_message["parse_mode"])
-            sleep(pidor_message["sleep_time"])
+    pidor_messages = dbase["pidorMessages"]
 
-        winner = choice(participants)
+    pidor_message = get_random_file(pidor_messages, {})
 
-        chats.update_one({"chat_id": chat.id, "personal_id": winner.user.id}, {"$inc": {"score": 1}})
-        bot.send_message(chat.id, pidor_message["winner_message"].format(winner.user.username),
-                         parse_mode=pidor_message["parse_mode"])
-
+    for text in pidor_message["intro_messages"]:
+        if text == "send_intro_sticker":
+            bot.send_sticker(chat.id, get_random_intro_sticker()["file_id"])
+        else:
+            bot.send_message(chat.id, text, parse_mode=pidor_message["parse_mode"])
         sleep(pidor_message["sleep_time"])
 
-        personal_sticker = get_random_personal_sticker(winner.user)
-        if personal_sticker is not None:
-            bot.send_sticker(chat.id, get_random_personal_sticker(winner.user)["file_id"])
+    winner = choice(participants)
+
+    chats.update_one({"chat_id": chat.id, "personal_id": winner.user.id}, {"$inc": {"score": 1}})
+    bot.send_message(chat.id, pidor_message["winner_message"].format(username_with_emoji(winner.user, chat)),
+                     parse_mode=pidor_message["parse_mode"])
+
+    timings.update_one({"chat_id": chat.id, "game": "pidor"}, {"$set": {"pidor_of_the_day": winner.user.id}})
+
+    sleep(pidor_message["sleep_time"])
+
+    personal_sticker = get_random_personal_sticker(winner.user)
+    if personal_sticker is not None:
+        bot.send_sticker(chat.id, get_random_personal_sticker(winner.user)["file_id"])
+
+
+@bot.message_handler(commands=["piu"])
+def echo_piu(message):
+    piu_messages = dbase["piuMessages"]
+    chats = dbase["chats"]
+    chat = message.chat
+    sender = message.from_user
+
+    if timings.find_one({"chat_id": chat.id, "game": "piu", "personal_id": sender.id}) is None:
+        timings.insert_one(
+            {"game": "piu", "chat_id": chat.id, "personal_id": sender.id, "last_run": None, "delta_hours": 0,
+             "delta_minutes": 3, "label_hours": ["час", "часа", "часов"],
+             "label_minutes": ["минуту", "минуты", "минут"], "label_seconds": ["секунду", "секунды", "секунд"]})
+
+    timing = timings.find_one({"chat_id": chat.id, "personal_id": sender.id, "game": "piu"})
+    current_date = datetime.datetime.now()
+
+    if timing["last_run"] is None:
+        timings.update_one({"chat_id": chat.id, "personal_id": sender.id, "game": "piu"},
+                           {"$set": {"last_run": date_to_tuple(current_date)}})
+    else:
+        last_run = datetime.datetime(*timing["last_run"])
+        delta = datetime.timedelta(hours=timing["delta_hours"], minutes=timing["delta_minutes"])
+
+        if last_run + delta > current_date:
+            sending_message, parse_mode = get_standard_answer("still_early_piu")
+            bot.send_message(chat.id, sending_message.format(
+                date_representation(last_run + delta - current_date, timing)), parse_mode=parse_mode)
+            return
+        else:
+            timings.update_one({"chat_id": chat.id, "personal_id": sender.id, "game": "piu"},
+                               {"$set": {"last_run": date_to_tuple(current_date)}})
+
+    piu_message = get_random_file(piu_messages, {})
+    sending_message, parse_mode = piu_message["message"], piu_message["parse_mode"]
+
+    receiver = message.text.split()[1] if len(message.text.split()) > 1 else sender.username
+    bot.send_message(chat.id, sending_message.format(username_with_emoji(sender, chat), receiver),
+                     parse_mode=parse_mode)
+
+    personal_sticker = get_random_personal_sticker(sender)
+    if personal_sticker is not None:
+        sleep(piu_message["sleep_time"])
+        bot.send_sticker(chat.id, get_random_personal_sticker(sender)["file_id"])
+
+    if receiver != sender.username:
+        chats.update_one({"chat_id": chat.id, "personal_id": sender.id}, {"$inc": {"piu_count": 1}})
+
+
+@bot.message_handler(commands=["pua"])
+def echo_pua(message):
+    pua_messages = dbase["puaMessages"]
+    chats = dbase["chats"]
+    chat = message.chat
+    sender = message.from_user
+
+    if timings.find_one({"chat_id": chat.id, "game": "pua", "personal_id": sender.id}) is None:
+        timings.insert_one(
+            {"game": "pua", "chat_id": chat.id, "personal_id": sender.id, "last_run": None, "delta_hours": 0,
+             "delta_minutes": 5, "label_hours": ["час", "часа", "часов"],
+             "label_minutes": ["минуту", "минуты", "минут"], "label_seconds": ["секунду", "секунды", "секунд"]})
+
+    timing = timings.find_one({"chat_id": chat.id, "personal_id": sender.id, "game": "pua"})
+    current_date = datetime.datetime.now()
+
+    if timing["last_run"] is None:
+        timings.update_one({"chat_id": chat.id, "personal_id": sender.id, "game": "pua"},
+                           {"$set": {"last_run": date_to_tuple(current_date)}})
+    else:
+        last_run = datetime.datetime(*timing["last_run"])
+        delta = datetime.timedelta(hours=timing["delta_hours"], minutes=timing["delta_minutes"])
+
+        if last_run + delta > current_date:
+            sending_message, parse_mode = get_standard_answer("still_early_pua")
+            bot.send_message(chat.id, sending_message.format(
+                date_representation(last_run + delta - current_date, timing)), parse_mode=parse_mode)
+            return
+        else:
+            timings.update_one({"chat_id": chat.id, "personal_id": sender.id, "game": "pua"},
+                               {"$set": {"last_run": date_to_tuple(current_date)}})
+
+    pua_message = get_random_file(pua_messages, {})
+    sending_message, parse_mode = pua_message["message"], pua_message["parse_mode"]
+
+    bot.send_message(chat.id, sending_message.format(username_with_emoji(sender, chat)), parse_mode=parse_mode)
+
+    chats.update_one({"chat_id": chat.id, "personal_id": sender.id}, {"$inc": {"pua_count": 1}})
 
 
 @bot.message_handler(commands=["pidorstats"])
@@ -126,15 +254,16 @@ def echo_pidorstats(message):
     stats_message = get_random_file(stats_messages, {"game": "pidor"})
 
     stats_list = "\n".join(
-        [stats_message["list_format"].format(i + 1, bot.get_chat_member(chat.id, personal_id).user.username, score,
-                                             declination_count(score))
+        [stats_message["list_format"].format(i + 1,
+                                             username_with_emoji(bot.get_chat_member(chat.id, personal_id).user, chat),
+                                             score, declination_count(score, "раз", "раза", "раз"))
          for i, personal_id, score in participants])
 
     if len([1 for i, personal_id, score in participants if score == participants[0][2]]) > 1:
         conclusion = stats_message["draft"]
     else:
         conclusion = stats_message["congratulation"].format(
-            bot.get_chat_member(chat.id, participants[0][1]).user.username)
+            username_with_emoji(bot.get_chat_member(chat.id, participants[0][1]).user, chat))
 
     bot.send_message(chat.id, stats_message["tab"].join((stats_message["message"], stats_list, conclusion)),
                      parse_mode=stats_message["parse_mode"])
@@ -156,14 +285,47 @@ def echo_piustats(message):
     stats_message = get_random_file(stats_messages, {"game": "piu"})
 
     stats_list = "\n".join(
-        [stats_message["list_format"].format(i + 1, bot.get_chat_member(chat.id, personal_id).user.username, score)
+        [stats_message["list_format"].format(i + 1,
+                                             username_with_emoji(bot.get_chat_member(chat.id, personal_id).user, chat),
+                                             score)
          for i, personal_id, score in participants])
 
     if len([1 for i, personal_id, score in participants if score == participants[0][2]]) > 1:
         conclusion = stats_message["draft"]
     else:
         conclusion = stats_message["congratulation"].format(
-            bot.get_chat_member(chat.id, participants[0][1]).user.username)
+            username_with_emoji(bot.get_chat_member(chat.id, participants[0][1]).user, chat))
+
+    bot.send_message(chat.id, stats_message["tab"].join((stats_message["message"], stats_list, conclusion)),
+                     parse_mode=stats_message["parse_mode"])
+
+
+@bot.message_handler(commands=["puastats"])
+def echo_puastats(message):
+    chats = dbase["chats"]
+    chat = message.chat
+    participants = [(i, participant["personal_id"], participant["pua_count"]) for i, participant in
+                    enumerate(chats.find({"chat_id": chat.id}).sort("pua_count", DESCENDING))]
+
+    if not participants:
+        sending_message, parse_mode = get_standard_answer("no_one_is_registered")
+        bot.send_message(chat.id, sending_message, parse_mode=parse_mode)
+        return
+
+    stats_messages = dbase["statsMessages"]
+    stats_message = get_random_file(stats_messages, {"game": "pua"})
+
+    stats_list = "\n".join(
+        [stats_message["list_format"].format(i + 1,
+                                             username_with_emoji(bot.get_chat_member(chat.id, personal_id).user, chat),
+                                             score)
+         for i, personal_id, score in participants])
+
+    if len([1 for i, personal_id, score in participants if score == participants[0][2]]) > 1:
+        conclusion = stats_message["draft"]
+    else:
+        conclusion = stats_message["congratulation"].format(
+            username_with_emoji(bot.get_chat_member(chat.id, participants[0][1]).user, chat))
 
     bot.send_message(chat.id, stats_message["tab"].join((stats_message["message"], stats_list, conclusion)),
                      parse_mode=stats_message["parse_mode"])
@@ -197,29 +359,23 @@ def echo_readpersonalstickers(message):
     chats.update_one({"chat_id": chat.id, "personal_id": sender.id}, {"$set": {"sticker_reading_mode": True}})
 
 
-@bot.message_handler(commands=["piu"])
-def echo_piu(message):
-    piu_messages = dbase["piuMessages"]
+@bot.message_handler(commands=["setemoji"])
+def echo_setemoji(message):
     chats = dbase["chats"]
     chat = message.chat
     sender = message.from_user
 
-    piu_message = get_random_file(piu_messages, {})
-    sending_message, parse_mode = piu_message["message"], piu_message["parse_mode"]
+    personal_emoji = text_has_emoji(message.text)
+    if personal_emoji is None:
+        return
+    chats.update_many({"personal_id": sender.id}, {"$set": {"emoji": personal_emoji}})
 
-    receiver = message.text.split()[1] if len(message.text.split()) > 1 else sender.username
-    bot.send_message(chat.id, sending_message.format(sender.username, receiver), parse_mode=parse_mode)
-
-    personal_sticker = get_random_personal_sticker(sender)
-    if personal_sticker is not None:
-        bot.send_sticker(chat.id, get_random_personal_sticker(sender)["file_id"])
-
-    if receiver != sender.username:
-        chats.update_one({"chat_id": chat.id, "personal_id": sender.id}, {"$inc": {"piu_count": 1}})
+    sending_message, parse_mode = get_standard_answer("emoji_supplied")
+    bot.send_message(chat.id, sending_message, parse_mode=parse_mode)
 
 
 @bot.message_handler(content_types=["sticker"])
-def echo_all(message):
+def echo_sticker(message):
     personal_stickers = dbase["personalStickers"]
     chats = dbase["chats"]
     sticker = message.sticker
--- utils/utils.py
+++ utils/utils.py
@@ -1,11 +1,34 @@
 from random import choice
+from emoji import UNICODE_EMOJI
+
+import datetime
 
 
-def declination_count(num):
+def declination_count(num, var1, var2, var3):
+    if num % 10 == 1 and not (num % 100 == 11):
+        return var1
     if 2 <= num % 10 <= 4 and not (12 <= num % 100 <= 14):
-        return "раза"
-    return "раз"
+        return var2
+    return var3
 
 
 def get_random_file(collection, local_filter):
     return choice([file for file in collection.find(local_filter)])
+
+
+def text_has_emoji(text):
+    for character in text:
+        if character in UNICODE_EMOJI:
+            return character
+    return None
+
+
+def date_representation(date, labels):
+    return " ".join(["{} {}".format(timing, declination_count(timing, var1, var2, var3)) for timing, var1, var2, var3 in
+                     [(date.seconds // 3600, *labels["label_hours"]),
+                      ((date.seconds // 60) % 60, *labels["label_minutes"]),
+                      (date.seconds % 60, *labels["label_seconds"])] if timing])
+
+
+def date_to_tuple(date):
+    return tuple(map(int, date.strftime("%Y %m %d %H %M %S").split()))
